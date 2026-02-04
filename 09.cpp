777995006ca3bca8ed085aa867fd12a79911fb35
siddhisavji24-cell <siddhisavji24@gmail.com>
2026-02-04 22:11:20 +0530

    Palindrome Number

---
+++ 09.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class Solution
+{
+public:
+    bool isPalindrome(int x) 
+    {
+        if (x < 0) return false;
+
+        int temp = x;
+        long rev = 0;  
+        while (x > 0)
+        {
+            int rem = x % 10;
+            rev = rev * 10 + rem;
+            x = x / 10;
+        }
+        return rev == temp;
+    }
+};
+int main()
+{
+    Solution s;
+    cout<<s.isPalindrome(121);
+}
